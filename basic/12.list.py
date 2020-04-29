181a9f508174e889e715b770076bafa0a4f49103
aniksahapritom <aniksaha.math47@gmail.com>
2020-04-29 17:29:34 +0600

    third commit

---
+++ basic/12.list.py
@@ -0,0 +1,9 @@
+listOne = ["apple","banana","charry","orange", "kiwi", "melon", "mango"]
+print(listOne)
+
+print(type(listOne))
+
+print(listOne[3:])  #from index 3 to last item
+
+print(listOne[-5:-1]) #Back index
+
